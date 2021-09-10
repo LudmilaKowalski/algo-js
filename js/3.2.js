5e7073219809cb804e5bf86ef02d579f0fdd79d4
Ludmila Kowalski <ludmila.kowalski@gmail.com>
2021-09-10 10:45:37 +0200

    exo 3

---
+++ js/3.2.js
@@ -0,0 +1,6 @@
+let arr = [1, 2, 3, 4, 5];
+
+let total =( arr[0] + arr[1]+ arr[2]+ arr[3]+ arr[4]);
+
+
+console.log( total / arr.length );
---
+++ js/3.3.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4, 5];
+
+arr.push("1, 2, 3, 4, 5")
+
+console.log (arr);
