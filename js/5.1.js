f997b598b6e744f2121183ea22d49f007424fe3f
Ludmila Kowalski <ludmila.kowalski@gmail.com>
2021-09-10 16:32:47 +0200

    fichier5

---
+++ js/5.1.js
@@ -0,0 +1,19 @@
+/*const readlineSync = require("readline-sync");
+let name= readlineSync.question("Whats your favorite series ?");
+let prod= readlineSync.question("When she was product ? ");
+let who= readlineSync.question("Who played in this series ?");
+*/
+
+const readlineSync = require("readline-sync");
+
+
+function askTvSerie() {
+    return a +b +c 
+}
+
+const a = readlineSync.question("Whats your favorite series ?");
+const b = readlineSync.question("When she was product ? ");
+const c = readlineSync.question("Who played in this series ?");
+const object = { a: a, b: b, c: c };
+
+console.log(object.a.b);
---
+++ js/5.2.js
@@ -0,0 +1,15 @@
+function randomizeCast(tvSerie)
+
+const readlineSync = require("readline-sync");
+
+
+function askTvSerie() {
+    return a +b +c 
+}
+
+const a = readlineSync.question("Whats your favorite series ?");
+const b = readlineSync.question("When she was product ? ");
+const c = readlineSync.question("Who played in this series ?");
+const object = { a: a, b: b, c: c };
+
+console.log(object.a.b);
