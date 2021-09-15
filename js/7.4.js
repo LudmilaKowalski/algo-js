83be7194fa4de2f56305171a6a3806d75da08590
Ludmila Kowalski <ludmila.kowalski@gmail.com>
2021-09-15 16:14:54 +0200

    fin des exos

--- js/7.4.js
+++ js/7.4.js
@@ -7,29 +7,33 @@ let text =("Hello! Welcome to the Pizza Flavors Manager.Please choose your actio
  let text5 = ("4 - Exit this program");
  let question = new Number(readlineSync.question("Hello! Welcome to the Pizza Flavors Manager. Please choose your actions: 1 - List all the pizza flavors 2 - Add a new pizza flavor 3 - Remove a pizza flavor 4 - Exit this program Enter your action's number:Enter your action's number:")); 
 
- let arr = [n]
+ let arr = []
+
+
 
  if (question ==1) {
     
-    console.log(arr +question); 
-  }
+    console.log(arr); 
+    let question = new Number(readlineSync.question("Hello! Welcome to the Pizza Flavors Manager. Please choose your actions: 1 - List all the pizza flavors 2 - Add a new pizza flavor 3 - Remove a pizza flavor 4 - Exit this program Enter your action's number:Enter your action's number:"));
+  
+}
 
  else if (question ==2)  {
-    let n = new Number(readlineSync.question("Give me a pizza taste"));
-    let arr = [n]
-    arr.push(n)
+    let newpizza = readlineSync.question("Give me a pizza taste ");
+    arr.push(newpizza)
+    let question = new Number(readlineSync.question("Hello! Welcome to the Pizza Flavors Manager. Please choose your actions: 1 - List all the pizza flavors 2 - Add a new pizza flavor 3 - Remove a pizza flavor 4 - Exit this program Enter your action's number:Enter your action's number:"));
     
   }
 
   else if (question ==3)  {
-    let e = new Number(readlineSync.question("Erased a pizza taste"));  
-    arr.pop(e); 
-    console.log("Too !");
-  }
-  else if (question ==4)  {
+    let removepizza = readlineSync.question("Pizza flavor: ");  
+    arr.pop(removepizza); 
+    let question = new Number(readlineSync.question("Hello! Welcome to the Pizza Flavors Manager. Please choose your actions: 1 - List all the pizza flavors 2 - Add a new pizza flavor 3 - Remove a pizza flavor 4 - Exit this program Enter your action's number:Enter your action's number:"));
 
-     new Error("Stop script");
   }
+  else {
+    return;
+}
 
 
   /*
---
+++ js/7.5.js
@@ -0,0 +1,5 @@
+let i = 1
+
+for (let i = 1; i <= 100; i += 1) {
+    console.log(i);
+  }
